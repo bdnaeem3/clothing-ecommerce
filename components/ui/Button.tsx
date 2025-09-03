ef9f09a45b16d7be3ceef5bee4b66585b6aa28af
Naeem Ahmed <bdnaeem3@gmail.com>
2025-09-03 06:30:22 +0600

    Button component added

---
+++ components/ui/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonProps } from "@/types";
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  type = "button",
+  variant = "primary",
+  className = "",
+  ...rest
+}) => {
+  const classMap: Record<NonNullable<ButtonProps["variant"]>, string> = {
+    primary:
+      "table px-[15px] py-[10px] text-white my-[10px] mx-auto bg-[#d73977] rounded-[4px] cursor-pointer",
+  };
+
+  return (
+    <button
+      type={type}
+      className={`${classMap[variant]} ${className}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
